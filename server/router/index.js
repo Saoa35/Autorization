763b1d0d2f7c7980a2048a732651327991d1a704
Saoa35 <saoamah@gmail.com>
2023-06-06 10:35:46 +0400

    + bcrypt, jsonwebtoken, uuid

--- server/router/index.js
+++ server/router/index.js
@@ -1,7 +1,5 @@
-const userController = require("../controllers/user-controller");
-
 const Router = require("express").Router;
-
+const userController = require("../controllers/user-controller");
 const router = new Router();
 
 router.post("/registration", userController.registration);
