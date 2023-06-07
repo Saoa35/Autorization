5cd920f5164dd1235d85765151b34fdf968fb290
Saoa35 <saoamah@gmail.com>
2023-06-07 09:43:03 +0400

    + express-validator libr.

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -4,12 +4,15 @@ const uuid = require("uuid");
 const mailService = require("./mail-service");
 const tokenService = require("./token-service");
 const UserDto = require("../dtos/user-dto");
+const ApiError = require("../exceptions/api-error");
 
 class UserService {
   async registration(email, password) {
     const candidate = await UserModel.findOne({ email });
     if (candidate) {
-      throw new Error(`User with email address ${email} already exists`);
+      throw ApiError.BadRequest(
+        `User with email address ${email} already exists`
+      );
     }
     const hashPassword = await bcrypt.hash(password, 3);
     const activationLink = uuid.v4();
@@ -33,10 +36,31 @@ class UserService {
   async activate(activationLink) {
     const user = await UserModel.findOne({ activationLink });
     if (!user) {
-      throw new Error("Wrong activation link");
+      throw ApiError.BadRequest("Wrong activation link");
     }
     user.isActivated = true;
     await user.save();
+  }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw ApiError.BadRequest("User with this email does not exist");
+    }
+    const isPassEquals = await bcrypt.compare(password, user.password);
+    if (!isPassEquals) {
+      throw ApiError.BadRequest("Wrong password");
+    }
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.refreshToken(refreshToken);
   }
 }
 
