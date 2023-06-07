d59001d9e2c2c28794fb2831eb51b720117469ad
Saoa35 <saoamah@gmail.com>
2023-06-07 15:39:29 +0400

    created AuthService, UserService, IUser

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
+import LoginForm from "./components/LoginForm";
 
 const App: FC = () => {
-  return <div className="App">AUTIRIZATION</div>;
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useState, useContext } from "react";
+import { Context } from "..";
+
+const LoginForm: FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { store } = useContext(Context);
+
+  return (
+    <div className="form">
+      <input
+        type="text"
+        value={email}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={() => store.login(email, password)}>Login</button>
+      <button onClick={() => store.registration(email, password)}>
+        Registration
+      </button>
+    </div>
+  );
+};
+
+export default LoginForm;
